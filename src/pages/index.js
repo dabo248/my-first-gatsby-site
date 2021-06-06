5dae06585980c0bc6c3690933fee008a197e2ad5
David Bochan <software@david.bo>
2021-06-06 15:59:39 +0200

    Update introduction text

--- src/pages/index.js
+++ src/pages/index.js
@@ -140,10 +140,11 @@ const IndexPage = () => {
       </h1>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
+        update in real-time..{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
           😎
         </span>
+        {" "}..hello World!
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
